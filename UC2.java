8df983fecae99b1351dfa3ae0e7d2089d5c72034
kaavyakathir <kaavyakathir@gmail.com>
2026-03-02 13:59:09 +0530

    uc2 implementation done

---
+++ UC2.java
@@ -0,0 +1,17 @@
+public class Main {
+    public class UC2PrintBanner {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****    ******    ***** ");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  ******    ***** ");
+        System.out.println("*     *  *     *  *              *");
+        System.out.println("*     *  *     *  *              *");
+        System.out.println(" *****    *****   *         ***** ");
+
+    }
+}
+    
+}
