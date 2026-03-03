60133294fd3c5244e20d3ef0e4b4486c4dc0aa05
kaavyakathir <kaavyakathir@gmail.com>
2026-03-03 14:29:22 +0530

    uc4 implementation done

---
+++ UC4.java
@@ -0,0 +1,15 @@
+ public class UC4 {
+
+    public static void main(String[] args) {
+        String[] banner = {
+                String.join("", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*"),
+                String.join("", "*", " ", "WELCOME TO OOPS", " ", "*"),
+                String.join("", "*", " ", "UC4 - ARRAY & LOOPS", " ", "*"),
+                String.join("", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*")
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
